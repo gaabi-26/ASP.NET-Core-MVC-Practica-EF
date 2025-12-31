3e980c6ad029c04d1274458743f3a87d3176db3a
Gabriel Martinez <gaabi26092005@gmail.com>
2025-12-31 19:48:39 -0300

    Crear modelos Elemento y Pokemon.

---
+++ Ejercicio1/Models/Elemento.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio1.Models
+{
+    public class Elemento
+    {
+        public int Id { get; set; }
+        [DisplayName("Descripción")]
+        [Required(ErrorMessage = "La descripción es requerida.")]
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Ejercicio1/Models/Pokemon.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Ejercicio1.Models
+{
+    public class Pokemon
+    {
+        public int Id { get; set; }
+        [DisplayName("Número")]
+        [Required(ErrorMessage = "El número es requerido.")]
+        public int Numero { get; set; }
+        [Required(ErrorMessage = "El nombre es requerido.")]
+        public string Nombre { get; set; }
+        [DisplayName("Descripción")]
+        public string? Descripcion { get; set; }
+        [DisplayName("Imagen")]
+        public string? UrlImagen { get; set; }
+        [Required(ErrorMessage = "El tipo es requerido.")]
+        public Elemento Tipo { get; set; }
+        [Required(ErrorMessage = "La debilidad es requerida.")]
+        public Elemento Debilidad { get; set; }
+        public bool Activo { get; set; }
+    }
+}
