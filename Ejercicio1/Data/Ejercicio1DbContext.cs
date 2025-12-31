28dab354cbda686c1d6f3c8b1fc87a1d668cb1ee
Gabriel Martinez <gaabi26092005@gmail.com>
2025-12-31 20:15:01 -0300

    Crear config inicial del DbContext.

---
+++ Ejercicio1/Data/Ejercicio1DbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ejercicio1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ejercicio1.Data
+{
+    public class Ejercicio1DbContext : DbContext
+    {
+        public Ejercicio1DbContext(DbContextOptions<Ejercicio1DbContext> options) : base(options)
+        {
+        }
+        public DbSet<Pokemon> Pokemons { get; set; }
+        public DbSet<Elemento> Elementos { get; set; }
+    }
+}
+    
